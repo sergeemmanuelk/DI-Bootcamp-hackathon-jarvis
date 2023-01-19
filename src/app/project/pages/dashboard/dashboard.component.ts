abab6d5a3c17eb2a8e4dd71a84f4dd3a9d8d3bb8
macbook <wkoua@oxylead.net>
2023-01-19 15:06:00 +0000

    feat : Ajout du reporting sur le dashboard utilisateur

--- src/app/project/pages/dashboard/dashboard.component.ts
+++ src/app/project/pages/dashboard/dashboard.component.ts
@@ -63,39 +63,5 @@ export class DashboardComponent implements OnInit {
       this.cards[2].title = this.report.hight;
     });
 
-
-
-  constructor(private _projectQuery : ProjectQuery) { }
-
-
-  ngOnInit(): void {
-
-    this._projectQuery.all$.subscribe((project) => {
-
-      this.recentActivities = project.issues.slice(0,5).filter((issue)=>{
-        return issue.priority == IssuePriority.HIGHEST
-      });
-
-      project.issues.forEach((issue) => {
-
-        if (issue.status === IssueStatusDisplay.Done) {
-          this.report.done = this.report.done + 1;
-        }
-         if (issue.status === IssueStatusDisplay.InProgress)
-        {
-          this.report.pending = this.report.pending + 1 ;
-        }
-
-        if (issue.priority === IssuePriority.HIGHEST) {
-          this.report.hight  = this.report.hight + 1;
-        }
-
-      });
-
-      this.cards[0].title = this.report.done;
-      this.cards[1].title = this.report.pending;
-      this.cards[2].title = this.report.hight;
-    });
   }
-
 }
